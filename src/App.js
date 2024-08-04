e191c07c060f5856880829037abe34ac227e735f
Samridhi Tandon <samridhitandon13@gmail.com>
2024-08-05 01:16:17 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header'
+import Banner from './Components/Banner';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Works from './Components/Works';
+import Certificates from './Components/Certificates'
+import Contact from './Components/Contact'
+import Mobile from './Components/Mobile'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mobile/>
+     <Header/>
+     <Banner />
+     <About/>
+     <Skills />
+     <Works/>
+     <Certificates/>
+     <Contact/>
     </div>
   );
 }
